24afb340591e9a4b3d0cabac49ff8de573f3afb3
Gabriel Szabo <szabo.gabriel@gmail.com>
2022-04-07 10:55:12 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,63 @@
-#!/bin/sh
+#!/bin/bash
 
-mvn clean install
+ver=$(cat pom.xml | grep version | head -n 1 | tr '>' '\n' | tr '<' '\n' | grep -v version)
 
-docker build --tag cgiserver .
+clean() {
+        if [ -e Dockerfile ]; then
+                rm -f Dockerfile
+        fi
+        if [ -e target ]; then
+                rm -rf target
+        fi
+}
+
+build() {
+        mvn clean install
+}
+
+package() {
+        cat Dockerfile.template | sed "s/##version##/${ver}/g" > Dockerfile
+
+        docker build --tag cgiserver .
+}
+
+help() {
+        cat<<EOF
+Build and package the application. Usage:
+
+./build.sh [clean|build|package|all]
+
+For example:
+
+./build.sh clean build package
+
+./build.sh all
+EOF
+}
+
+main() {
+        if [ "x$1" = "x" ]; then
+                help
+                exit 0
+        fi
+        if [ "$1" = "all" ]; then
+                clean
+                build
+                install
+                exit 0
+        fi
+        if [ "$1" = "clean" ]; then
+                clean
+                shift
+        fi
+        if [ "$1" = "build" ]; then
+                build
+                shift
+        fi
+        if [ "$1" = "package" ]; then
+                build
+                shift
+        fi
+}
+
+main $@

a827915d5ac5b9ee21e91723cd87fe4bf12b7ff3
Gabriel Szabo <gszabo@li-f8a00a4c-21d6-11b2-a85c-836e340c7047.ibm.com>
2025-05-14 09:51:53 +0200

    Update go version

--- src-go/server.go
+++ src-go/server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"net/http"
-	"net/http/cgi"
+        "log"
+        "net/http"
+        "net/http/cgi"
+        "path/filepath"
 )
 
-func cgiHandler(w http.ResponseWriter, r *http.Request) {
-	handler := cgi.Handler{Path: "./cgi-bin/index"}
-	handler.ServeHTTP(w, r)
-}
-
 func main() {
-	http.HandleFunc("/", cgiHandler)
-	http.ListenAndServe(":8080", nil)
+        // Serve CGI scripts from ./cgi-bin/
+        http.Handle("/", http.StripPrefix("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                scriptPath := filepath.Join("/home/gszabo/Projects/Temp/cgiserver/cgi-bin", r.URL.Path)
+                h := &cgi.Handler{
+                        Path: scriptPath,
+                        Dir:  "./cgi-bin",
+                }
+                h.ServeHTTP(w, r)
+        })))
+
+        log.Println("Serving CGI on http://localhost:8080")
+        log.Fatal(http.ListenAndServe(":8080", nil))
 }
